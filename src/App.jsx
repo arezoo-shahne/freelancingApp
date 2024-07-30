4fc39bba23240fcc9ff3a0aae49f1265626f1eb1
arezoo shahne <arezoo.shahne@yahoo.com>
2024-07-30 18:12:57 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='font-bold text-primary-500 opacity-85'>عنوان پروژه</h1>
+    </div>
+  )
+}
+
+export default App
